36f75e92340054ed67f9a33cacdc3d6751b59f73
Yuvraj Sahni <yuvrajsahni3@gmail.com>
2024-02-03 03:38:06 +0530

    minor changes

---
+++ .bash_history
@@ -0,0 +1,9 @@
+git add .
+git commit -m "inital commit"
+git config --global user.email "you@example.com"
+git config --global user.email "yuvrajsahni3@gmail.com"
+git config --global user.name "Yuvraj Sahni"
+git commit -m "inital commit"
+git branch -M main
+git remote add origin https://github.com/YuvrajSahni/Virtual-Mouse.git
+git push -u origin main
--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,3 +1,10 @@
+"""
+Hand Tracing Module
+By: Murtaza Hassan
+Youtube: http://www.youtube.com/c/MurtazasWorkshopRoboticsandAI
+Website: https://www.computervision.zone/
+"""
+
 import cv2
 import mediapipe as mp
 import time
@@ -6,14 +13,15 @@ import numpy as np
 
 
 class handDetector():
-    def __init__(self, mode=False, maxHands=1, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.trackCon = trackCon
 
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+                                        self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
 
