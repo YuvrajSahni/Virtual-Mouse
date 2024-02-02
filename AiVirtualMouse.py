0f79f7ab0d4245f8c85b98068a23ddcaf47f650e
Yuvraj Sahni <yuvrajsahni3@gmail.com>
2024-02-03 03:30:24 +0530

    inital commit

---
+++ AiVirtualMouse.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+import HandTrackingModule as htm
+import time
+import  autopy
+
+######################
+wCam, hCam = 640,480
+######################
+
+cap = cv2.VideoCapture(0)
+cap.set(3,wCam)
+cap.set(4,hCam)
+pTime=0
+detector = htm.handDetector(maxHands=1)
+
+while True:
+    #1. Find hand landmarks
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList, bbox = detector.findPosition(img)
+
+
+    #2. Get the tip of the index and middle finger
+    #3. Check which fingers are up
+    #4. Only index: moving mode
+    #5. Convert Coordinates
+    #6. Smoothen values
+    #7. Move mouse
+    #8. Both index and middle fingers are up: Clicking mode
+    #9. Find distance between fingers
+    #10. Click mouse if distance is short
+
+    #11. Frame Rate
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20,50), cv2.FONT_HERSHEY_PLAIN,3, (255,0,0), 3)
+
+    #12. Display
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
